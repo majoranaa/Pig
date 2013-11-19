072bebae3a4fc472d58baf6cc5fc80ff2060a9f4
majoranaa <kai.yang412@gmail.com>
2013-11-18 20:51:40 -0800

    Fixed node

--- web.js
+++ web.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var exec = require('child_process').exec;
+
+function execute(command){
+    exec(command, function(error, stdout, stderr){ console.log(stdout); });
+};
+
 app.use(express.logger());
+app.use(express.static(__dirname + '/www'));
 
 app.get('/', function(request, response) {
     var content = fs.readFileSync('index.html').toString('utf-8');
     response.send(content);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
