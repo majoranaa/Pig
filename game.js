44d1118b29af07bce74987cb95d5309a59fe6cf6
majoranaa <kai.yang412@gmail.com>
2013-11-19 21:18:23 -0800

    Added socket.io

---
+++ game.js
@@ -0,0 +1,68 @@
+function endTurn() {
+    player[current] += score;
+    var turnRes = document.createTextNode(turn + ") " + score + " points. Total: " + player[current]);
+    document.getElementById("player" + current).appendChild(turnRes);
+    document.getElementById("player" + current).appendChild(document.createElement('br'));
+    document.getElementById("currentRoll").innerHTML = "";
+    document.getElementById("turnScore").innerHTML = "";
+    hold.style.display = "block";
+    roll.setAttribute('value', 'Roll');    
+    if (player[current] > 100) {
+	var win = document.createElement('h1');
+	win.innerHTML = "Player " + current + " wins!!!";
+	win.setAttribute('id', 'winner');
+	document.getElementById("currentPlayer").style.display = "none";
+	hold.style.display = "none";
+	roll.style.display = "none";
+	document.getElementById("container").appendChild(win);
+	return;
+    }
+
+    hold.style.display = "block";
+    roll.setAttribute('value', 'Roll');    
+    
+    if (current == 1) {
+	current = 2;
+    } else {
+	current = 1;
+	turn++;
+    }
+    document.getElementById("currentPlayer").innerHTML = "Player " + current;
+    score = 0;
+    next = false;    
+}
+
+function go() {
+    if (next) {
+	endTurn();
+    }
+    
+    var dice1 = Math.floor((Math.random()*6)+1);
+    var dice2 = Math.floor((Math.random()*6)+1);
+    
+    var trial = document.createTextNode(dice1 + " and " + dice2);
+    
+    if (dice1 == 1 && dice2 == 1) {
+	player[current] = 0;
+	score = 0;
+	document.getElementById("currentRoll").innerHTML = "Oh that sucks. You rolled two ones. Your total score is now zero. Other player now proceed";	
+    } else if (dice1 == 1 || dice2 == 1) {
+	score = 0;
+	document.getElementById("currentRoll").innerHTML = "Nice try. You rolled a one. Your turn score is now zero. Other player now proceed";
+    } else {
+	score += (dice1 + dice2);
+    }
+    
+    if (score == 0) {
+	hold.style.display = "none";
+	document.getElementById("turnScore").innerHTML = "";
+	roll.setAttribute('value', 'Roll');    
+	next = true;
+	return;
+    }
+
+    roll.setAttribute('value', 'Roll Again');
+    document.getElementById("currentRoll").appendChild(trial);
+    document.getElementById("currentRoll").appendChild(document.createElement('br'));
+    document.getElementById("turnScore").innerHTML = score;
+}   
--- web.js
+++ web.js
@@ -1,21 +1,28 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var exec = require('child_process').exec;
+var routes = require('./routes');
 
-function execute(command){
-    exec(command, function(error, stdout, stderr){ console.log(stdout); });
-};
 
 app.use(express.logger());
 app.use(express.static(__dirname + '/www'));
 
-app.get('/', function(request, response) {
-    var content = fs.readFileSync('index.html').toString('utf-8');
-    response.send(content);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+var server = app.listen(port, function() {
     console.log("Listening on " + port);
+});
+
+var io = require('socket.io').listen(server);
+var clients = [];
+io.sockets.on('connection', function(client) {
+    console.log("New connection", client.id);
+    clients.push(client.id);
 });
